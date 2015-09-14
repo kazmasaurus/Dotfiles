98900659bfc74e0d75647b705e6ef9822ed26592
Zak Remer <zak.remer@gmail.com>
2015-09-13 22:29:50 -0700

    Updates simnuke for Xcode 7 things.
    - Correctly handles `(unavailable, runtime profile not found)` in the device list
    - Doesn't try to mix and match iOS and watchOS devices/runtimes

--- Xcode/simnuke.rb
+++ Xcode/simnuke.rb
@@ -1,15 +1,16 @@
 #!/usr/bin/env ruby
 
+# Originally based on https://gist.github.com/cabeca/cbaacbeb6a1cc4683aa5
 device_types_output = `xcrun simctl list devicetypes`
 device_types = device_types_output.scan /(.*) \((.*)\)/
-puts "Device types: #{device_types}"
+# puts "Device types: #{device_types}"
 
 runtimes_output = `xcrun simctl list runtimes`
 runtimes = runtimes_output.scan /(.*) \(.*\) \((com.apple[^)]+)\)$/
-puts "Runtimes: #{runtimes}"
+# puts "Runtimes: #{runtimes}"
 
 devices_output = `xcrun simctl list devices`
-devices = devices_output.scan /\s\s\s\s(.*) \(([^)]+)\) (.*)/
+devices = devices_output.scan /\s\s\s\s(.*) \(([A-Z0-9-]+)\) (.*)/
 
 devices.each do |device|
   puts "Removing device #{device[0]} (#{device[1]})"
@@ -18,9 +19,10 @@ end
 
 device_types.each do |device_type|
   runtimes.each do |runtime|
-    puts "Creating #{device_type} with #{runtime}"
-    command = "xcrun simctl create '#{device_type[0]}' #{device_type[1]} #{runtime[1]}"
-    puts "Command: #{command}"
+    next if !(device_type[0] =~ /[wW]atch/).nil? ^ !(runtime[0] =~ /[wW]atch/).nil?
+    puts "Creating #{device_type[0]} with #{runtime[0]}"
+    command = "xcrun simctl create '#{device_type[0]}' #{device_type[1]} #{runtime[1]} 2> /dev/null"
+    # puts "Command: #{command}"
     command_output = `#{command}`
     sleep 0.5
   end
