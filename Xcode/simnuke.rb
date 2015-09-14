e4d007ff180a24b2e5f7900ce529dce93ddd7c0b
Zak Remer <zak.remer@gmail.com>
2015-09-13 22:58:29 -0700

    Cleans up output and adds error reporting to simnuke.

--- Xcode/simnuke.rb
+++ Xcode/simnuke.rb
@@ -3,27 +3,23 @@
 # Originally based on https://gist.github.com/cabeca/cbaacbeb6a1cc4683aa5
 device_types_output = `xcrun simctl list devicetypes`
 device_types = device_types_output.scan /(.*) \((.*)\)/
-# puts "Device types: #{device_types}"
 
 runtimes_output = `xcrun simctl list runtimes`
 runtimes = runtimes_output.scan /(.*) \(.*\) \((com.apple[^)]+)\)$/
-# puts "Runtimes: #{runtimes}"
 
 devices_output = `xcrun simctl list devices`
 devices = devices_output.scan /\s\s\s\s(.*) \(([A-Z0-9-]+)\) (.*)/
 
 devices.each do |device|
-  puts "Removing device #{device[0]} (#{device[1]})"
-  `xcrun simctl delete #{device[1]}`
+  success = `xcrun simctl delete #{device[1]}`
+  puts "#{$?.exitstatus == 0 ? "Removed" : "Couldn't remove"} #{device[0].lstrip} (#{device[1]})"
 end
 
 device_types.each do |device_type|
   runtimes.each do |runtime|
     next if !(device_type[0] =~ /[wW]atch/).nil? ^ !(runtime[0] =~ /[wW]atch/).nil?
-    puts "Creating #{device_type[0]} with #{runtime[0]}"
     command = "xcrun simctl create '#{device_type[0]}' #{device_type[1]} #{runtime[1]}"
-    puts "Command: #{command}"
-    command_output = `#{command}`
-    sleep 0.5
+    uuid = `#{command}`.rstrip
+    puts "#{$?.exitstatus == 0 ? "Created" : "Couldn't create"} #{device_type[0]} with #{runtime[0]} (#{uuid})"
   end
 end
