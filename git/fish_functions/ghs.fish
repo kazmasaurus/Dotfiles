e87b592123e9fddff7e7c8dd5c9276c26837b4a7
Zak Remer <zak.remer@gmail.com>
2019-03-15 15:21:46 -0700

    Add allowing args to `ghs`.

--- git/fish_functions/ghs.fish
+++ git/fish_functions/ghs.fish
@@ -1,5 +1,5 @@
 
 # A Stripped down history showing how the current branch compares against development
 function ghs
-    gh --first-parent --boundary --simplify-by-decoration development...
+    gh --first-parent --boundary --simplify-by-decoration development... $argv
 end
