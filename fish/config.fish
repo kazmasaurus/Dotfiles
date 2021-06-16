0d07e801b8b1b962d5a556498c084e48fabfa894
Zak Remer <zak.remer@gmail.com>
2021-06-15 17:25:32 -0700

    Fix `hub` alias.

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,10 @@
 
-# Wrap hub in `git`
-eval (hub alias -s)
-
 # Path party
+# Important that Path party kicks off the night, otherwise later commands
+# may not have access to anything we expect to be installed through `brew`
 set PATH "/usr/local/sbin" $PATH
 set PATH "/opt/homebrew/bin" $PATH
+
+# Wrap hub in `git`
+eval (hub alias -s)
 
